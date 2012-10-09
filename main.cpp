060d17f667662caf5417c1c6096719ce926f2826
Darelist <darelist@gmail.com>
2012-10-09 18:10:52 +0300

    Apsirasiau algoritmo kintamuosius + papildziau gitignore

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main()
 {
     cout << "MoteCarlo realizacijos pradzia" << endl;
+    //kiontamuju apsirashymas
+    double f,x;   // j-jos reiksmes ir argumentas x
+    int n;      // uzdavinio dimensija
     return 0;
 }
