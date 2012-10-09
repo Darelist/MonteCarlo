12650128dda4627bd9f86d15cbd72db34fe31e49
Darelist <darelist@gmail.com>
2012-10-09 19:53:56 +0300

    realizuotas random 0-1 intervale

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
     cout << "MoteCarlo realizacijos pradzia" << endl;
-    cout << "(RANDOM SEARCH METHOD) << endl;
+    cout << "(RANDOM SEARCH METHOD)" << endl;
     //kiontamuju apsirashymas
-    double f,x;   // j-jos reiksmes ir argumentas x
+    double f,*x, h=1,l=0;   // j-jos reiksmes ir argumentas x
     int n;      // uzdavinio dimensija
     int xSprendin; //  sprendinys
     int fSprend;    // uzdavinio sprendinys
+    x=(double*) malloc(510);
+    cout<<"Atsakymas: "<<endl;
+    for(int i=0; i<100; i++){
+    x[i]=rand()*(h-l)/RAND_MAX +l ;
+    cout<<x[i]<<endl;
+}
+
     return 0;
 }
