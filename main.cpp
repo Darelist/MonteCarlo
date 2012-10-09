b6a15bfef55bb772ec4b44737c6e225cf631fb98
Darelist <darelist@gmail.com>
2012-10-09 18:39:12 +0300

    pasitvarkiau aprasyma

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
     cout << "MoteCarlo realizacijos pradzia" << endl;
+    cout << "(RANDOM SEARCH METHOD) << endl;
     //kiontamuju apsirashymas
     double f,x;   // j-jos reiksmes ir argumentas x
     int n;      // uzdavinio dimensija
+    int xSprendin; // argumento sprendinys
     return 0;
 }
